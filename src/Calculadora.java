22f396c74e629cb1bf654e4fd60a28eae802f584
Juan Carlos <Juancarmgc2017@gmail.com>
2023-05-29 11:01:04 +0200

    hjugyuguygg

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -3,10 +3,19 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import java.awt.Font;
+import java.awt.Color;
+import javax.swing.JTextField;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.awt.event.ActionEvent;
 
 public class Calculadora extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -28,12 +37,133 @@ public class Calculadora extends JFrame {
 	 * Create the frame.
 	 */
 	public Calculadora() {
+		ArrayList<Integer> numer = new ArrayList<Integer>();
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 627, 421);
 		contentPane = new JPanel();
+		contentPane.setForeground(new Color(0, 0, 0));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lbEtiquetaPrincipal = new JLabel("CALCULADORA");
+		lbEtiquetaPrincipal.setFont(new Font("Stencil", Font.PLAIN, 17));
+		lbEtiquetaPrincipal.setBounds(218, 10, 130, 43);
+		contentPane.add(lbEtiquetaPrincipal);
+		
+		JButton bt1_0 = new JButton("1");
+		bt1_0.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				numer.add(1);
+				
+			}
+		});
+		bt1_0.setForeground(new Color(255, 255, 255));
+		bt1_0.setBackground(new Color(0, 0, 0));
+		bt1_0.setBounds(51, 93, 53, 32);
+		contentPane.add(bt1_0);
+		
+		JButton bt1_1 = new JButton("4");
+		bt1_1.setForeground(Color.WHITE);
+		bt1_1.setBackground(Color.BLACK);
+		bt1_1.setBounds(51, 137, 53, 32);
+		contentPane.add(bt1_1);
+		
+		JButton bt1_2 = new JButton("7");
+		bt1_2.setForeground(Color.WHITE);
+		bt1_2.setBackground(Color.BLACK);
+		bt1_2.setBounds(51, 179, 53, 32);
+		contentPane.add(bt1_2);
+		
+		JButton bt1_3 = new JButton("2");
+		bt1_3.setForeground(Color.WHITE);
+		bt1_3.setBackground(Color.BLACK);
+		bt1_3.setBounds(131, 93, 53, 32);
+		contentPane.add(bt1_3);
+		
+		JButton bt1_4 = new JButton("5");
+		bt1_4.setForeground(Color.WHITE);
+		bt1_4.setBackground(Color.BLACK);
+		bt1_4.setBounds(131, 137, 53, 32);
+		contentPane.add(bt1_4);
+		
+		JButton bt1_5 = new JButton("8");
+		bt1_5.setForeground(Color.WHITE);
+		bt1_5.setBackground(Color.BLACK);
+		bt1_5.setBounds(131, 179, 53, 32);
+		contentPane.add(bt1_5);
+		
+		JButton bt1_6 = new JButton("3");
+		bt1_6.setForeground(Color.WHITE);
+		bt1_6.setBackground(Color.BLACK);
+		bt1_6.setBounds(206, 93, 53, 32);
+		contentPane.add(bt1_6);
+		
+		JButton bt1_7 = new JButton("6");
+		bt1_7.setForeground(Color.WHITE);
+		bt1_7.setBackground(Color.BLACK);
+		bt1_7.setBounds(206, 137, 53, 32);
+		contentPane.add(bt1_7);
+		
+		JButton bt1_8 = new JButton("9");
+		bt1_8.setForeground(Color.WHITE);
+		bt1_8.setBackground(Color.BLACK);
+		bt1_8.setBounds(206, 179, 53, 32);
+		contentPane.add(bt1_8);
+		
+		JButton bt1_9 = new JButton("+");
+		bt1_9.setForeground(Color.WHITE);
+		bt1_9.setBackground(Color.BLACK);
+		bt1_9.setBounds(367, 93, 53, 32);
+		contentPane.add(bt1_9);
+		
+		JButton bt1_10 = new JButton("-");
+		bt1_10.setForeground(Color.WHITE);
+		bt1_10.setBackground(Color.BLACK);
+		bt1_10.setBounds(367, 137, 53, 32);
+		contentPane.add(bt1_10);
+		
+		JButton btnX = new JButton("x");
+		btnX.setForeground(Color.WHITE);
+		btnX.setBackground(Color.BLACK);
+		btnX.setBounds(367, 179, 53, 32);
+		contentPane.add(btnX);
+		
+		JButton bt1_12 = new JButton("/");
+		bt1_12.setForeground(Color.WHITE);
+		bt1_12.setBackground(Color.BLACK);
+		bt1_12.setBounds(367, 221, 53, 32);
+		contentPane.add(bt1_12);
+		
+		JButton bt1_13 = new JButton("0");
+		bt1_13.setForeground(Color.WHITE);
+		bt1_13.setBackground(Color.BLACK);
+		bt1_13.setBounds(206, 221, 53, 32);
+		contentPane.add(bt1_13);
+		
+		JButton bt1_14 = new JButton(",");
+		bt1_14.setForeground(Color.WHITE);
+		bt1_14.setBackground(Color.BLACK);
+		bt1_14.setBounds(51, 221, 53, 32);
+		contentPane.add(bt1_14);
+		
+		JButton bt1_15 = new JButton("=");
+		bt1_15.setForeground(Color.WHITE);
+		bt1_15.setBackground(Color.BLACK);
+		bt1_15.setBounds(367, 263, 53, 32);
+		contentPane.add(bt1_15);
+		
+		textField = new JTextField();
+		textField.setBounds(51, 52, 452, 31);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JButton bt1_15_1 = new JButton("<<");
+		bt1_15_1.setForeground(Color.WHITE);
+		bt1_15_1.setBackground(Color.BLACK);
+		bt1_15_1.setBounds(449, 93, 53, 32);
+		contentPane.add(bt1_15_1);
 	}
 
 }
